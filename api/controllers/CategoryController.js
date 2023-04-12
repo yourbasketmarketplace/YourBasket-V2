119b50768f745d6395de50edb71599c85e04aca2
pawan <pawan@gmail.com>
2023-04-12 17:02:22 +0530

    changes

--- api/controllers/CategoryController.js
+++ api/controllers/CategoryController.js
@@ -1,153 +1,142 @@
 const Category = require('../models/Category');
 
 const CategoryController = () => {
-    const create = async (req, res) => {
-        // body is part of a form-data
-        const {
-            body
-        } = req;
+  const create = async (req, res) => {
+    // body is part of a form-data
+    const {
+      body,
+    } = req;
 
-        try {
-            const category = await Category.create({
-                name: body.name
-            });
+    try {
+      const category = await Category.create({
+        name: body.name,
+      });
 
-            if (!category) {
-                return res.status(400).json({
-                    msg: 'Bad Request: Model not found'
-                });
-            }
+      if (!category) {
+        return res.status(400).json({
+          msg: 'Bad Request: Model not found',
+        });
+      }
 
-            return res.status(200).json({
-                category
-            });
-        } catch (err) {
-            // better save it to log file
-            console.error(err);
+      return res.status(200).json({
+        category,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        msg: 'Internal server error',
+      });
+    }
+  };
 
-            return res.status(500).json({
-                msg: 'Internal server error'
-            });
-        }
-    };
+  const getAll = async (req, res) => {
+    try {
+      const categories = await Category.findAll();
+      return res.status(200).json({
+        categories,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        msg: 'Internal server error',
+      });
+    }
+  };
 
-    const getAll = async (req, res) => {
-        try {
-            const categories = await Category.findAll();
-            return res.status(200).json({
-                categories
-            });
-        } catch (err) {
-            console.log(err);
-            return res.status(500).json({
-                msg: 'Internal server error'
-            });
-        }
-    };
+  const get = async (req, res) => {
+    // params is part of an url
+    const { id } = req.params;
 
-    const get = async (req, res) => {
-        // params is part of an url
-        const id = req.params.id;
+    try {
+      const category = await Category.findOne({
+        where: {
+          id,
+        },
+      });
 
-        try {
-            const category = await Category.findOne({
-                where: {
-                    id,
-                },
-            });
+      if (!category) {
+        return res.status(400).json({
+          msg: 'Bad Request: Model not found',
+        });
+      }
 
-            if (!category) {
-                return res.status(400).json({
-                    msg: 'Bad Request: Model not found'
-                });
-            }
-
-            return res.status(200).json({
-                category
-            });
-        } catch (err) {
-            // better save it to log file
-            console.error(err);
-
-            return res.status(500).json({
-                msg: 'Internal server error'
-            });
-        }
-    };
+      return res.status(200).json({
+        category,
+      });
+    } catch (err) {
+      // better save it to log file
+      return res.status(500).json({
+        msg: 'Internal server error',
+      });
+    }
+  };
 
 
-    const update = async (req, res) => {
-        // params is part of an url
-        const id = req.params.id;
+  const update = async (req, res) => {
+    // params is part of an url
+    const { id } = req.params;
 
-        // body is part of form-data
-        const {
-            body
-        } = req;
+    // body is part of form-data
+    const {
+      body,
+    } = req;
 
-        try {
-            const category = await Category.findById(id);
+    try {
+      const category = await Category.findById(id);
 
-            if (!category) {
-                return res.status(400).json({
-                    msg: 'Bad Request: Model not found'
-                });
-            }
+      if (!category) {
+        return res.status(400).json({
+          msg: 'Bad Request: Model not found',
+        });
+      }
 
-            const updatedCategory = await category.update({
-                name: body.name,
-            });
+      const updatedCategory = await category.update({
+        name: body.name,
+      });
 
-            return res.status(200).json({
-                updatedCategory
-            });
-
-        } catch (err) {
-            // better save it to log file
-            console.error(err);
-
-            return res.status(500).json({
-                msg: 'Internal server error'
-            });
-        }
-    };
+      return res.status(200).json({
+        updatedCategory,
+      });
+    } catch (err) {
+      // better save it to log file
+      return res.status(500).json({
+        msg: 'Internal server error',
+      });
+    }
+  };
 
 
-    const destroy = async (req, res) => {
-        // params is part of an url
-        const id = req.params.id;
+  const destroy = async (req, res) => {
+    // params is part of an url
+    const { id } = req.params;
 
-        try {
-            const category = Category.findById(id);
+    try {
+      const category = Category.findById(id);
 
-            if (!category) {
-                return res.status(400).json({
-                    msg: 'Bad Request: Model not found'
-                })
-            }
+      if (!category) {
+        return res.status(400).json({
+          msg: 'Bad Request: Model not found',
+        });
+      }
 
-            await category.destroy();
+      await category.destroy();
 
-            return res.status(200).json({
-                msg: 'Successfully destroyed model'
-            });
-        } catch (err) {
-            // better save it to log file
-            console.error(err);
+      return res.status(200).json({
+        msg: 'Successfully destroyed model',
+      });
+    } catch (err) {
+      // better save it to log file
+      return res.status(500).json({
+        msg: 'Internal server error',
+      });
+    }
+  };
 
-            return res.status(500).json({
-                msg: 'Internal server error'
-            });
-        }
-    };
-
-    return {
-        create,
-        getAll,
-        get,
-        update,
-        destroy
-    };
-}
+  return {
+    create,
+    getAll,
+    get,
+    update,
+    destroy,
+  };
+};
 
 module.exports = CategoryController;
--- api/models/Category.js
+++ api/models/Category.js
@@ -5,28 +5,31 @@ const sequelize = require('../../config/database');
 const tableName = 'categories';
 
 const Category = sequelize.define('Category', {
-    name: {
-        type: Sequelize.STRING,
-        unique: false,
-    },
-    type: {
-        type: Sequelize.DECIMAL,
-        allowNull: false,
-        defaultValue: '0'
-    },
-    parent_id: {
-        type: Sequelize.DECIMAL,
-        allowNull: false,
-        defaultValue: '0'
-    }
+  name: {
+    type: Sequelize.STRING,
+    unique: false,
+  },
+  imaage: {
+    type: Sequelize.STRING,
+  },
+  type: {
+    type: Sequelize.DECIMAL,
+    allowNull: false,
+    defaultValue: '0',
+  },
+  parent_id: {
+    type: Sequelize.DECIMAL,
+    allowNull: false,
+    defaultValue: '0',
+  },
 }, {
-    tableName
+  tableName,
 });
 
 // eslint-disable-next-line
 Category.prototype.toJSON = function () {
-    const values = Object.assign({}, this.get());
-    return values;
+  const values = Object.assign({}, this.get());
+  return values;
 };
 
 module.exports = Category;
