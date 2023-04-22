b0f4726e7ecb517c89b84c5ecd3cd93bf25ccea0
pawan <pawan@gmail.com>
2023-04-22 11:22:18 +0530

    changes

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -131,6 +131,7 @@ const PageController = () => {
         data,
       });
     } catch (err) {
+      console.log(err)
       // better save it to log file
       return res.status(500).json({
         msg: 'Internal server error',
