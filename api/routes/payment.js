6d40a20c7c7b8835f3d59092bc151cca24f834ea
pawan <pawan@gmail.com>
2023-05-24 08:22:00 +0530

    changes

--- api/routes/payment.js
+++ api/routes/payment.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const OrderController = require('../controllers/OrderController');
 const auth = require('../policies/auth.policy');
 
-router.post('/ipn', (req, res) => OrderController().pesaPalIpn(req, res));
+router.get('/ipn', (req, res) => OrderController().pesaPalIpn(req, res));
 
 module.exports = router;
